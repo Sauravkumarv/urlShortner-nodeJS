a9d34b7e731d4a5d8ce29627d6912bfebd5f683d
Saurav Kumar Verma <121866975+Sauravkumarv@users.noreply.github.com>
2025-07-04 13:52:26 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const urlRoute = require("./routes/url");
+const { connectTOMongoDB } = require("./connect");
+const URL = require("./models/url");
+
+const app = express();
+
+const port = 8001;
+
+connectTOMongoDB("mongodb://localhost:27017/short-url").then(() =>
+  console.log("Mongodb connected")
+);
+
+app.use(express.json());
+
+app.use("/url", urlRoute);
+
+app.get("/:shortId", async (req, res) => {
+  const shortId = req.params.shortId;
+  const entry = await URL.findOneAndUpdate(
+    {
+      shortId,
+    },
+    {
+      $push: {
+        visitHistory: { timestamp: Date.now() },
+      },
+    }
+  );
+  res.redirect(entry.redirectURL);
+});
+
+app.listen(port, () => {
+  console.log(`Server Started at port${port}`);
+});
